b94828c9bc4e588ecde48f747ecf7b02ad5d78aa
luksab <luksablp@gmail.com>
2021-08-09 21:57:20 +0200

    add github ssh keys

--- configuration.nix
+++ configuration.nix
@@ -39,9 +39,17 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.lukas = {
     isNormalUser = true;
+    home = "/home/lukas";
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
     shell = pkgs.fish;
+    openssh.authorizedKeys.keyFiles = [
+      (builtins.fetchurl {
+        url = "https://github.com/luksab.keys";
+        #sha256 = "sha256:0lhvhdrzp2vphqhkcgl34xzn0sill6w7mgq8xh1akm1z1rsvd9v4";
+      })
+    ];
   };
+  nix.allowedUsers = [ "lukas" ];
 
   programs.fish.enable = true;
 
