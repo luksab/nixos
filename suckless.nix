5d44a28d2becb3c94c4bf698818cf55693b77437
luksab <luksablp@gmail.com>
2021-08-10 21:02:37 +0200

    add custom dwmblocks config

--- suckless.nix
+++ suckless.nix
@@ -5,7 +5,7 @@ let larbs = pkgs.callPackage ./pkgs/larbs_scripts.nix { };
 in {
   # Enable touchpad support (enabled default in most desktopManager).
   services.xserver.libinput.enable = true;
-  
+
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
@@ -27,6 +27,14 @@ in {
     (dwmblocks.overrideAttrs (oldAttrs: rec {
       src = builtins.fetchTarball {
         url = "https://github.com/LukeSmithxyz/dwmblocks/archive/master.tar.gz";
+        configFile = writeText "config.def.h" (builtins.readFile "${
+            fetchFromGitHub{
+              owner = "luksab";
+              repo = "nixos";
+              rev = "b402a94d865ae5ddc18278119de203f1d6eec4fb";
+              sha256 = "f066f53a852e6fdde975eee0ecbac25f6659bfe061e4b2890b8cefdd033b953b";
+            }
+          }/config.h");
       };
     }))
 
