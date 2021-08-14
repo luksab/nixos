13044dc452319c4ee0c2e54d9179e0c590bbece9
luksab <luksablp@gmail.com>
2021-08-14 18:33:18 +0200

    build overviewer.py

--- pkgs/overviewer.nix
+++ pkgs/overviewer.nix
@@ -1,23 +1,42 @@
-{ stdenv }:
+{ stdenv, pkgs }:
 
 stdenv.mkDerivation rec {
   name = "overviewer-${version}";
-  version = "2021-05-08";
+  version = "v0.17.0";
 
-  srcs = [ builtins.fetchTarball {
-    url = "https://github.com/overviewer/Minecraft-Overviewer/archive/master.tar.gz";
-  }
+  srcs = [
+    (pkgs.fetchFromGitHub {
+      owner = "overviewer";
+      repo = "Minecraft-Overviewer";
+      rev = version;
+      name = name;
+      sha256 = "sha256:1hqbd6m850qqs9ck54j2gf54kjhn4zr3as65rpx9rglmjx8ymh4j";
+    })
+    (pkgs.fetchFromGitHub {
+      owner = "python-pillow";
+      repo = "Pillow";
+      rev = "8.3.1";
+      name = "pillow";
+      sha256 = "sha256:0nbl7w813c7k8wpmldlgipqnn9bdzrjgxyg256zqlx9nxcfmaalm";
+    })
   ];
 
-  
+  sourceRoot = name;
+
+  buildInputs = with pkgs.python39Packages; [ pkgs.python39 numpy pillow ];
+  propagatedBuildInputs = with pkgs.python39Packages; [ pkgs.python39 numpy pillow ];
+
+  preBuild = ''
+    chmod -R u+w ../pillow
+    ln -s ../pillow .
+    make ../pillow
+  '';
 
   installPhase = ''
+    export PIL_INCLUDE_DIR=./pillow/src/libImaging
+    python3.9 setup.py build
     mkdir -p $out/bin/
-    cp -r .local/bin/* $out/bin/
-    cp -r .local/bin/statusbar/* $out/bin/
-    ls -lah $out
+    cp -r ./overviewer_core/ $out/bin/
+    cp ./overviewer.py $out/bin/
   '';
-    #   mkdir -p $out/share/zsh/site-functions
-    # cp pure.zsh $out/share/zsh/site-functions/prompt_pure_setup
-    # cp async.zsh $out/share/zsh/site-functions/async
 }
