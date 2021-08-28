61c0441a67ee0ee0e03f4bbd7092ab41449ac756
luksab <luksablp@gmail.com>
2021-08-28 15:01:46 +0200

    move overridden suckless packages to overlays

--- modules/suckless/default.nix
+++ modules/suckless/default.nix
@@ -9,7 +9,6 @@ in {
   };
 
   config = mkIf cfg.enable {
-    
 
     # system.activationScripts = {
     #   text = ''
@@ -20,46 +19,16 @@ in {
 
     environment.systemPackages = with pkgs; [
       larbs_scripts
+      dwm
+      dmenu
+      st
+      dwmblocks
 
       pamixer
       maim
       xclip
       notify-desktop
-      dwm
-
-      (dwmblocks.overrideAttrs (oldAttrs: rec {
-        src = pkgs.fetchFromGitHub {
-          owner = "LukeSmithxyz";
-          repo = "dwmblocks";
-          rev = "66f31c307adbdcc2505239260ecda24a49eea7af";
-          sha256 = "sha256-j3wCRyl1+0D2XcdqhE5Zgf53bEXhcaU4dvdyYG9LZ2g=";
-        };
-        patches = [ ./dwmblocks.patch ];
-      }))
-
       lm_sensors
-
-      (dmenu.overrideAttrs (oldAttrs: rec {
-        src = pkgs.fetchFromGitHub {
-          owner = "LukeSmithxyz";
-          repo = "dmenu";
-          rev = "3a6bc67fbd6df190b002d33f600a6465cad9cfb8";
-          sha256 = "sha256-qwOcJqYGMftFwayfYA3XM0xaOo6ALV4gu1HpFRapbFg=";
-        };
-      }))
-
-      (st.overrideAttrs (oldAttrs: rec {
-        buildInputs = oldAttrs.buildInputs ++ [ git harfbuzz ];
-        src = pkgs.fetchFromGitHub {
-          owner = "LukeSmithxyz";
-          repo = "st";
-          rev = "e053bd6036331cc7d14f155614aebc20f5371d3a";
-          sha256 = "sha256-WwjuNxWoeR/ppJxJgqD20kzrn1kIfgDarkTOedX/W4k=";
-          name = "st";
-        };
-        patches = [ ./st.patch ];
-        fetchSubmodules = true;
-      }))
     ];
   };
 }
--- overlays/default.nix
+++ overlays/default.nix
@@ -19,6 +19,7 @@ self: super: {
   youtube-dl = self.unstable.youtube-dl;
   zoom-us = self.unstable.zoom-us;
 
+  # suckless packages
   dwm = (super.dwm.overrideAttrs (oldAttrs: rec {
     src = super.pkgs.fetchFromGitHub {
       owner = "luksab";
@@ -27,5 +28,37 @@ self: super: {
       sha256 = "sha256-+eXQeqC5OTJ9YS0SR9N39ekeaiiIEgvDDY+hJyfWChs=";
       name = "dwm";
     };
+  }));
+
+  dwmblocks = (super.dwmblocks.overrideAttrs (oldAttrs: rec {
+    src = super.pkgs.fetchFromGitHub {
+      owner = "LukeSmithxyz";
+      repo = "dwmblocks";
+      rev = "66f31c307adbdcc2505239260ecda24a49eea7af";
+      sha256 = "sha256-j3wCRyl1+0D2XcdqhE5Zgf53bEXhcaU4dvdyYG9LZ2g=";
+    };
+    patches = [ ../modules/suckless/dwmblocks.patch ];
+  }));
+
+  dmenu = (super.dmenu.overrideAttrs (oldAttrs: rec {
+    src = super.pkgs.fetchFromGitHub {
+      owner = "LukeSmithxyz";
+      repo = "dmenu";
+      rev = "3a6bc67fbd6df190b002d33f600a6465cad9cfb8";
+      sha256 = "sha256-qwOcJqYGMftFwayfYA3XM0xaOo6ALV4gu1HpFRapbFg=";
+    };
+  }));
+
+  st = (super.st.overrideAttrs (oldAttrs: rec {
+    buildInputs = oldAttrs.buildInputs ++ [ super.pkgs.git super.pkgs.harfbuzz ];
+    src = super.pkgs.fetchFromGitHub {
+      owner = "LukeSmithxyz";
+      repo = "st";
+      rev = "e053bd6036331cc7d14f155614aebc20f5371d3a";
+      sha256 = "sha256-WwjuNxWoeR/ppJxJgqD20kzrn1kIfgDarkTOedX/W4k=";
+      name = "st";
+    };
+    patches = [ ../modules/suckless/st.patch ];
+    fetchSubmodules = true;
   }));
 }
