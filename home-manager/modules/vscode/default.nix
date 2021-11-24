49238a55e0c13942a71caff33db96883d97ee280
luksab <lukas@sabatschus.de>
2021-11-24 17:16:38 +0100

    add wgsl vs-extensions

--- home-manager/modules/vscode/default.nix
+++ home-manager/modules/vscode/default.nix
@@ -18,21 +18,31 @@ in {
     programs.vscode = {
       enable = true;
       package = pkgs.vscode;
-      extensions = with pkgs.vscode-extensions; [
-        # arrterian.nix-env-selector
-        bbenoist.nix
-        brettm12345.nixfmt-vscode
-        ms-vscode-remote.remote-ssh
-        matklad.rust-analyzer
-        # polymeilex.wgsl
-        (lib.mkIf (config.luksab.arch == "x86_64") pkgs.vsliveshare-new)
-        brettm12345.nixfmt-vscode
-        tomoki1207.pdf
-        ms-python.python
-        
-        ms-vscode.cpptools
-        esbenp.prettier-vscode
-      ];
+      extensions = with pkgs.vscode-extensions;
+        [
+          # arrterian.nix-env-selector
+          bbenoist.nix
+          brettm12345.nixfmt-vscode
+          ms-vscode-remote.remote-ssh
+          matklad.rust-analyzer
+          (lib.mkIf (config.luksab.arch == "x86_64") pkgs.vsliveshare-new)
+          brettm12345.nixfmt-vscode
+          tomoki1207.pdf
+          ms-python.python
+
+          ms-vscode.cpptools
+          esbenp.prettier-vscode
+        ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
+          name = "wgsl";
+          publisher = "polymeilex";
+          version = "0.1.11";
+          sha256 = "sha256-u1vZ+iQASORBGa/Ck9bXz45iUQaL0Xc0tDLlAN5l6Ow=";
+        }] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [{
+          name = "wgsl-analyzer";
+          publisher = "wgsl-analyzer";
+          version = "0.1.0";
+          sha256 = "sha256-Oat/axLsM7kz/16oA3ZtKS1ygg5yenatnwOE3HHjYI4=";
+        }];
     };
   };
 }
