47102962746f6adc17874137290aecf232ebab4d
luksab <lukas@sabatschus.de>
2021-12-02 13:32:08 +0100

    update ssh keys

--- users/root.nix
+++ users/root.nix
@@ -4,7 +4,7 @@
     openssh.authorizedKeys.keyFiles = [
       (builtins.fetchurl {
         url = "https://github.com/luksab.keys";
-        sha256 = "sha256:14n4lfw94w7pl0qp70c2ajdzfd7dv35fskspay6kmbp42b3s6wvf";
+        sha256 = "sha256:0826wzh9nmrjp256ixxyr6p2qch1vzx7d9zqmd95bm02k5vk8m00";
       })
     ];
   };
