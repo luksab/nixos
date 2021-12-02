4ff258de7a248b35cfb0870796a595aaa7a6ca42
luksab <lukas@sabatschus.de>
2021-12-02 18:22:22 +0100

    fix qtbase?

--- overlays/default.nix
+++ overlays/default.nix
@@ -16,7 +16,7 @@ self: super: {
   #multimc = super.qt5.callPackage ../packages/multimc { };
   multimc = self.unstable.multimc.override { msaClientID = "d4434167-7a48-4be7-b463-647b1580e072"; };
   obs-studio-plugins.obs-ndi = super.obs-studio-plugins.obs-ndi.overrideAttrs (old: {
-    buildInputs = [ self.unstable.obs-studio super.qtbase self.ndi ];
+    buildInputs = [ self.unstable.obs-studio super.qt5.qtbase self.ndi ];
   });
   obs = (self.wrapOBS {
     plugins = with self.unstable.obs-studio-plugins; [
