8b90f1ee9d3a2696b6eb34838ce05f32f27343fd
luksab <lukas@sabatschus.de>
2022-01-17 14:36:41 +0100

    update shortcut

--- packages/shortcut/default.nix
+++ packages/shortcut/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   pname = "shortcut";
-  version = "7f9905adfd26952394c7d6c99086f26d0a899f66";
+  version = "7e2847fe5d6b5e3199fb15dae2496b4d24832972";
 
   src = fetchFromGitHub {
     owner = "luksab";
     repo = "Shortcut";
     rev = version;
-    sha256 = "sha256-+O4rcWBvoLmYzrBm6xXAYqmuGTqDHN4Xr/Q+UmT7IsY=";
+    sha256 = "sha256-cAkc0mBLA0b8k6aVsTjUSP1MuPg/jrj3ThcpyynUT9M=";
   };
 
   installPhase = ''
