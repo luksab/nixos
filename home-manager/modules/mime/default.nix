946bcaf245d4ad7f1351f4c03b20b2fda9b1d341
luksab <lukas@sabatschus.de>
2022-01-27 23:27:10 +0100

    add csv code mime type

--- home-manager/modules/mime/default.nix
+++ home-manager/modules/mime/default.nix
@@ -8,46 +8,48 @@ in {
 
   config = mkIf cfg.enable {
     xdg.mimeApps = {
-        enable = true;
-        defaultApplications = {
-            "application/x-zip"= [ "org.gnome.FileRoller.desktop" ];
-            "application/zip"= [ "org.gnome.FileRoller.desktop" ];
-            "image/png" = [ "feh.desktop" ];
-            "image/jpeg" = [ "feh.desktop" ];
-            "x-scheme-handler/http=" = [ "brave-browser.desktop" ];
-            "x-scheme-handler/https=" = [ "brave-browser.desktop" ];
-            "x-scheme-handler/chrome" = [ "brave-browser.desktop" ];
-            "text/html" = [ "brave-browser.desktop" ];
-            # echo $XDG_DATA_DIRS
-            /*text/html=brave-browser.desktop
-            x-scheme-handler/http=brave-browser.desktop
-            x-scheme-handler/https=brave-browser.desktop
-            x-scheme-handler/about=brave-browser.desktop
-            x-scheme-handler/unknown=brave-browser.desktop
-            x-scheme-handler/chrome=userapp-Firefox-AZQ280.desktop
-            application/x-extension-htm=userapp-Firefox-AZQ280.desktop
-            application/x-extension-html=userapp-Firefox-AZQ280.desktop
-            application/x-extension-shtml=userapp-Firefox-AZQ280.desktop
-            application/xhtml+xml=userapp-Firefox-AZQ280.desktop
-            application/x-extension-xhtml=userapp-Firefox-AZQ280.desktop
-            application/x-extension-xht=userapp-Firefox-AZQ280.desktop
-            x-scheme-handler/mailto=brave-browser.desktop
-            application/zip=org.gnome.FileRoller.desktop
-            application/x-zip=org.gnome.FileRoller.desktop*/
-            /*[Added Associations]
-            x-scheme-handler/http=userapp-Firefox-AZQ280.desktop;
-            x-scheme-handler/https=userapp-Firefox-AZQ280.desktop;
-            x-scheme-handler/chrome=userapp-Firefox-AZQ280.desktop;
-            text/html=userapp-Firefox-AZQ280.desktop;
-            application/x-extension-htm=userapp-Firefox-AZQ280.desktop;
-            application/x-extension-html=userapp-Firefox-AZQ280.desktop;
-            application/x-extension-shtml=userapp-Firefox-AZQ280.desktop;
-            application/xhtml+xml=userapp-Firefox-AZQ280.desktop;
-            application/x-extension-xhtml=userapp-Firefox-AZQ280.desktop;
-            application/x-extension-xht=userapp-Firefox-AZQ280.desktop;
-            application/x-shellscript=nautilus-autorun-software.desktop;
-            */
-        };
+      enable = true;
+      defaultApplications = {
+        "application/x-zip" = [ "org.gnome.FileRoller.desktop" ];
+        "application/zip" = [ "org.gnome.FileRoller.desktop" ];
+        "image/png" = [ "feh.desktop" ];
+        "image/jpeg" = [ "feh.desktop" ];
+        "x-scheme-handler/http=" = [ "brave-browser.desktop" ];
+        "x-scheme-handler/https=" = [ "brave-browser.desktop" ];
+        "x-scheme-handler/chrome" = [ "brave-browser.desktop" ];
+        "text/html" = [ "brave-browser.desktop" ];
+        "text/csv" = [ "code.desktop" ];
+        # echo $XDG_DATA_DIRS
+        /* text/html=brave-browser.desktop
+           x-scheme-handler/http=brave-browser.desktop
+           x-scheme-handler/https=brave-browser.desktop
+           x-scheme-handler/about=brave-browser.desktop
+           x-scheme-handler/unknown=brave-browser.desktop
+           x-scheme-handler/chrome=userapp-Firefox-AZQ280.desktop
+           application/x-extension-htm=userapp-Firefox-AZQ280.desktop
+           application/x-extension-html=userapp-Firefox-AZQ280.desktop
+           application/x-extension-shtml=userapp-Firefox-AZQ280.desktop
+           application/xhtml+xml=userapp-Firefox-AZQ280.desktop
+           application/x-extension-xhtml=userapp-Firefox-AZQ280.desktop
+           application/x-extension-xht=userapp-Firefox-AZQ280.desktop
+           x-scheme-handler/mailto=brave-browser.desktop
+           application/zip=org.gnome.FileRoller.desktop
+           application/x-zip=org.gnome.FileRoller.desktop
+        */
+        /* [Added Associations]
+           x-scheme-handler/http=userapp-Firefox-AZQ280.desktop;
+           x-scheme-handler/https=userapp-Firefox-AZQ280.desktop;
+           x-scheme-handler/chrome=userapp-Firefox-AZQ280.desktop;
+           text/html=userapp-Firefox-AZQ280.desktop;
+           application/x-extension-htm=userapp-Firefox-AZQ280.desktop;
+           application/x-extension-html=userapp-Firefox-AZQ280.desktop;
+           application/x-extension-shtml=userapp-Firefox-AZQ280.desktop;
+           application/xhtml+xml=userapp-Firefox-AZQ280.desktop;
+           application/x-extension-xhtml=userapp-Firefox-AZQ280.desktop;
+           application/x-extension-xht=userapp-Firefox-AZQ280.desktop;
+           application/x-shellscript=nautilus-autorun-software.desktop;
+        */
+      };
     };
   };
 }
