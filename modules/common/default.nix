e48ea80ade90c4c3d2bd20971c21e9397c4d088f
luksab <lukas@sabatschus.de>
2022-03-11 16:48:48 +0100

    add perf to kernel

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -19,7 +19,7 @@ in {
 
     mayniklas.var.mainUser = "lukas";
 
-    environment.systemPackages = with pkgs; [ git nixfmt usbutils pciutils ];
+    environment.systemPackages = with pkgs; [ git nixfmt usbutils pciutils config.boot.kernelPackages.perf ];
 
     programs.mtr.enable = true;
 
