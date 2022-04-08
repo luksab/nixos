1c76efb32648e4c78087f958ad2c1f3404c9d85e
luksab <lukas@sabatschus.de>
2022-04-08 21:25:12 +0200

    update olafArm

--- krops.nix
+++ krops.nix
@@ -63,7 +63,7 @@ in rec {
 
   majaArm = createHost "majaArm" "root@val.luksab.de";
   rapaArm = createHost "rapaArm" "root@152.70.59.189";
-  olafArm = createHost "olafArm" "root@158.101.211.244";
+  olafArm = createHost "olafArm" "root@olaf.sabatschus.de";
 
   pi4b = createHost "pi4b" "root@192.168.178.55";
 
