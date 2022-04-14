4fc1421f86d9ad5afe2f5a4449f6810f3e41d98f
luksab <lukas@sabatschus.de>
2022-04-14 21:35:34 +0200

    fix pi4b for 22.05

--- modules/pi4b/default.nix
+++ modules/pi4b/default.nix
@@ -23,8 +23,6 @@ in {
         };
         # Use the extlinux boot loader. (NixOS wants to enable GRUB by default)
         grub.enable = false;
-        # Enables the generation of /boot/extlinux/extlinux.conf
-        generic-extlinux-compatible.enable = true;
       };
 
       kernelPackages = pkgs.linuxPackages_rpi4;
