5a239aacfeec850b58d57b1a77477b3741e83450
luksab <lukas@sabatschus.de>
2022-04-24 23:32:17 +0200

    nix update, remove deprecated pulseaudio-module-bt

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -9,7 +9,6 @@ in {
     sound.enable = true;
     hardware.pulseaudio = {
       enable = true;
-      extraModules = [ pkgs.pulseaudio-modules-bt ];
       package = pkgs.pulseaudioFull;
     };
 
