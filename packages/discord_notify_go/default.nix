5cd06577c2d126b50a7e5d33a23dc853e9ac421d
luksab <lukas@sabatschus.de>
2022-08-08 21:19:29 +0200

    update discord notify bot

--- packages/discord_notify_go/default.nix
+++ packages/discord_notify_go/default.nix
@@ -5,8 +5,8 @@ buildGoModule {
   src = fetchFromGitHub {
     owner = "luksab";
     repo = "discord_notify_go";
-    rev = "4fe8b6b0c8232692a61194865335340233e53b79";
-    sha256 = "sha256-2bbflmN8qQlWwtNf+Tv9/LEDEAoH5XzEx3F5vMl0u6c=";
+    rev = "caf47f0b2a4ba96659204abc2cbe00545d64098f";
+    sha256 = "sha256-75OgMGkgKrQ1Ml9WA/nrNF+RsPfNtk1bF/MUq1tsVW0=";
   };
-  vendorSha256 = "sha256-RaWIa8SFbnf+YVWml8PTkRfkSOeKUYC+jQz9qqLXeJo=";
+  vendorSha256 = "sha256-tMKasxp1C7AElQMhW3MTZPLw4WWuXtlOiwntmVG9bLY=";
 }
