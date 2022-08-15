c16577453056cf1589126b41ba215152dea3707c
luksab <lukas@sabatschus.de>
2022-08-15 21:53:17 +0200

    blacklist raydium to make touchscreen work

--- machines/x86/crimson/hardware.nix
+++ machines/x86/crimson/hardware.nix
@@ -5,6 +5,8 @@
   # acpi_backlight=none allows the backlight save/load systemd service to work.
   boot.kernelParams = [ "amdgpu.backlight=0" "acpi_backlight=none" ];
 
+  boot.blacklistedKernelModules = [ "raydium_i2c_ts" ];
+
   # Wifi support
   hardware.firmware = [ pkgs.rtw89-firmware ];
 
