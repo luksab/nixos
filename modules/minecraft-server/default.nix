fa452c6598d92a3619b4080501d8f76bad894174
luksab <lukas@sabatschus.de>
2022-09-25 16:52:36 +0200

    add optimizations to minecraft server

--- modules/minecraft-server/default.nix
+++ modules/minecraft-server/default.nix
@@ -76,7 +76,7 @@ in {
 
         WorkingDirectory = "${cfg.dataDir}";
         ExecStart =
-          "${cfg.javaPackage}/bin/java -Xmx16G -Xms4G -jar ${cfg.serverFile}";
+          "${cfg.javaPackage}/bin/java -Xms16G -Xmx16G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -jar ${cfg.serverFile}";
       };
 
       environment = {
