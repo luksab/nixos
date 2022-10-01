d110353be5af30d76dd778317dcb75bcc825e3e8
luksab <lukas@sabatschus.de>
2022-10-01 22:54:03 +0200

    enable minecraft server on arm

--- machines/aarch64/arm/configuration.nix
+++ machines/aarch64/arm/configuration.nix
@@ -27,7 +27,7 @@
     };
 
     minecraft-server = {
-      enable = false;
+      enable = true;
       openFirewall = true;
     };
 
@@ -116,19 +116,10 @@
       rulesetFile = ./ruleset.nft;
     };
   };
-  # networking.firewall.allowPing = true;
-  # networking.firewall.allowedTCPPorts = [ 22 80 443 25565 ];
-  # networking.firewall.allowedUDPPorts = [ 19132 ];
 
   # Ports for syncthing
-  networking.firewall.allowedTCPPorts = [
-    8384
-    22000
-  ];
-  networking.firewall.allowedUDPPorts = [
-    22000
-    21027
-  ];
+  networking.firewall.allowedTCPPorts = [ 8384 22000 ];
+  networking.firewall.allowedUDPPorts = [ 22000 21027 ];
 
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQD0rI0jOEIGoY3kQR5ErapABGPPCXv10OrBBTpniqOxdKc8d56f69/24LxmLTeSjo6VuME6Y4CcdEKl8PnVrp1kaAsqfMbjzzU/W7hGPUeTYutu69tgnWXc6g9Vf/oTzGgclY5TDZ1+QA9+wNiNdLxd2J9pzuVzyISHlO7sn8Vk+8rpV6r/MgCUYNVQvWDYi3jEu1Mp9YXn28rvG1pMuvn5hT28jZYC9A9TNFGtAb9BtVpRNWMDPMnlD6VdH8utBVb16yAD3DTY+Orb0TWjsrQQ7utMqrBulPyjD1//mTQhKggSww4lgn/sLzmi5xxgAGKFUn+N579bdlI4c7M+ZqWpHIJE3IXH2ux+iUjypcTBNgXpfS5neDVo08fE56QWLMcoqHOACi6p1jwK+6GaDSJpySwus2nj1vC7KXbSGZWJYCNSliuQOsqd/lXPt/q2qwADLl+2uy/jPy1iCYJrd8WsjZi98m2VSsGY+Z99a1GSZ3tEvqxn4IZQx9p1aizaDt0= lukas@desktop"
